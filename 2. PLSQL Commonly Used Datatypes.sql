25f6cd4f20ddf37116279aed82c6960bdc373d19
jothiprakash <jothiprakashanandan@gmail.com>
2018-07-21 17:17:33 +0530

    PLSQL datatypes updated

--- 2. PLSQL Commonly Used Datatypes.sql
+++ 2. PLSQL Commonly Used Datatypes.sql
@@ -121,3 +121,62 @@ BEGIN
 END;
 /
 
+
+--Timezone Data Types
+DECLARE
+  l_tsmp TIMESTAMP(2);
+  l_tsmp_tz TIMESTAMP(2) WITH TIME ZONE;
+  l_tsmp_new TIMESTAMP(2);
+  l_tsmp_tz_new TIMESTAMP(2) WITH TIME ZONE;
+  l_int INTERVAL DAY(2) TO SECOND(2) := '7 00:00:00.00'; -- INTERVAL '7' DAY;
+BEGIN
+  l_tsmp := TO_TIMESTAMP('02-NOV-2013 10:00:00.00','DD-MON-RRRR HH24:MI:SS.FF');
+  
+  l_tsmp_tz := TO_TIMESTAMP_TZ('02-NOV-2013 10:00:00.00 PST PDT','DD-MON-RRRR HH24:MI:SS.FF TZR TZD');  
+  l_tsmp_tz := TO_TIMESTAMP_TZ('02-NOV-2013 10:00:00.00 America/Los_Angeles PDT','DD-MON-RRRR HH24:MI:SS.FF TZR TZD');
+  
+  -- Add 7 days
+  l_tsmp_new := l_tsmp + l_int ;
+  l_tsmp_tz_new := l_tsmp_tz + l_int;
+  
+  DBMS_OUTPUT.PUT_LINE('New Timestamp is '||TO_CHAR(l_tsmp_new,'DD-MON-RRRR HH24:MI:SS.FF'));
+  DBMS_OUTPUT.PUT_LINE('New Timestamp with timezone is '||TO_CHAR(l_tsmp_tz_new,'DD-MON-RRRR HH24:MI:SS.FF TZR TZD'));
+    
+END;
+
+
+--RECORDS DATA TYPE
+DECLARE
+  TYPE emp_rec is RECORD(emp_name   VARCHAR2(60),
+                         dept_id    departments.dept_id%TYPE,
+                         loc        VARCHAR2(10) DEFAULT 'CA');
+  l_emprec emp_rec;
+BEGIN
+  l_emprec .emp_name := 'John';
+  l_emprec .dept_id := 10;
+  
+  DBMS_OUTPUT.PUT_LINE('Employee Name is ' || l_emprec .emp_name);
+  DBMS_OUTPUT.PUT_LINE('Employee Location is ' || l_emprec .loc);
+END;
+
+
+--%ROWTYPE 
+DECLARE
+  l_dept_rec departments%ROWTYPE;
+BEGIN
+  l_dept_rec.dept_id := 10;
+  DBMS_OUTPUT.PUT_LINE('Department id is ' || l_dept_rec.dept_id);
+END;
+
+
+--NESTED RECORDS
+DECLARE
+  TYPE employee_rec IS RECORD(emp_name  VARCHAR2(60),
+                              deptrec   departments%ROWTYPE,
+                              loc       VARCHAR2(10) DEFAULT 'CA');
+  l_employee_rec employee_rec;
+BEGIN
+  l_employee_rec.deptrec.dept_id := 20;
+  
+  DBMS_OUTPUT.PUT_LINE('Employee Dept is ' || l_employee_rec.deptrec.dept_id);
+END;
